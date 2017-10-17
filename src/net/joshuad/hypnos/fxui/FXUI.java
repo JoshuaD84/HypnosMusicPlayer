c06262407b5d3fd681379bd7dc2e55117b92656d
Joshua Hartwell <Joshua@JoshuaD.net>
2017-10-17 03:15:46 -0400

    Darken split pane dividers in dark theme.

--- src/net/joshuad/hypnos/fxui/FXUI.java
+++ src/net/joshuad/hypnos/fxui/FXUI.java
@@ -31,7 +31,6 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
